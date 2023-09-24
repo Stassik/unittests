41fd924dd033000a2750dbb1063b2c2918b636dc
unknown <nastase4ka@mail.ru>
2023-09-24 23:54:42 +0300

    add new project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+public class Main {
+    public static void main(String[] args) {
+
+
+    Calculator newCalculator = new Calculator(10, 1.5);
+    System.out.println(newCalculator.calculateDiscount());
+
+
+    }
+
+
+
+
+}
